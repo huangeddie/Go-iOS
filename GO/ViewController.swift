10a6018981ddcf8c24c92a83e257c5c0fbd2ce35
Eddie Huang <aigagror@gmail.com>
2018-07-29 13:30:17 -0500

    Initial Commit

---
+++ GO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GO
+//
+//  Created by Eddie Huang on 7/29/18.
+//  Copyright © 2018 Eddie Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
