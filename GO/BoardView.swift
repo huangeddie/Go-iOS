fcf95006b4d0dcc14c04a2897fee2cf385ce8631
Eddie Huang <aigagror@gmail.com>
2018-07-29 14:17:41 -0500

    basic version

---
+++ GO/BoardView.swift
@@ -0,0 +1,81 @@
+//
+//  BoardView.swift
+//  GO
+//
+//  Created by Eddie Huang on 7/29/18.
+//  Copyright © 2018 Eddie Huang. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+public class BoardView: UIView {
+    @IBInspectable var boardColor: UIColor = UIColor.brown
+    @IBInspectable var dimension: Int = 19
+    @IBInspectable var dots: Bool = false
+    
+    private var numberOfSquares: Int {
+        return dimension - 1
+    }
+    
+    var gridSize = CGSize()
+    
+    public override func draw(_ rect: CGRect) {
+        boardColor.setFill()
+        let background = UIBezierPath(roundedRect: bounds, cornerRadius: 0)
+        background.fill()
+        
+        let gridSize = CGSize(width: bounds.width * 0.9, height: bounds.height * 0.9)
+
+        let grid = UIBezierPath()
+        for i in 0...numberOfSquares {
+            grid.move(to: CGPoint(x: (bounds.width - gridSize.width) / 2, y: (bounds.height - gridSize.height) / 2 + CGFloat(i) * gridSize.height / CGFloat(numberOfSquares)))
+            grid.addLine(to: CGPoint(x: (bounds.width - gridSize.width) / 2 + gridSize.width, y: (bounds.height - gridSize.height) / 2 + CGFloat(i) * gridSize.height / CGFloat(numberOfSquares)))
+
+            grid.move(to: CGPoint(x: (bounds.width - gridSize.width) / 2 + CGFloat(i) * gridSize.width / CGFloat(numberOfSquares), y: (bounds.height - gridSize.height) / 2))
+            grid.addLine(to: CGPoint(x: (bounds.width - gridSize.width) / 2 + CGFloat(i) * gridSize.width / CGFloat(numberOfSquares), y: (bounds.height - gridSize.height) / 2 + gridSize.height))
+        }
+        UIColor.black.setStroke()
+
+
+        // create the dots
+        if (dots) {
+            UIColor.black.setFill()
+            var w = gridSize.width / 3
+            var h = gridSize.height / 3
+            let s = CGSize(width: gridSize.width / 100, height: gridSize.width / 100)
+            switch numberOfSquares {
+            case 12:
+                w = gridSize.width / 4
+                h = gridSize.height / 4
+            case 8:
+                w = gridSize.width / 4
+                h = gridSize.height / 4
+            default:
+                break
+            }
+
+            let arr = [
+                CGRect(origin: CGPoint(x: (bounds.width - s.width) / 2, y: (bounds.height - s.height) / 2), size: s),
+                CGRect(origin: CGPoint(x: (bounds.width - s.width) / 2 - w, y: (bounds.height - s.height) / 2 + h), size: s),
+                CGRect(origin: CGPoint(x: (bounds.width - s.width) / 2 + w, y: (bounds.height - s.height) / 2 + h), size: s),
+                CGRect(origin: CGPoint(x: (bounds.width - s.width) / 2 - w, y: (bounds.height - s.height) / 2 - h), size: s),
+                CGRect(origin: CGPoint(x: (bounds.width - s.width) / 2 + w, y: (bounds.height - s.height) / 2 - h), size: s)
+            ]
+
+            for i in arr {
+                UIBezierPath(ovalIn: i).fill()
+            }
+        }
+
+
+
+
+        grid.fill()
+
+
+
+        grid.stroke()
+        self.gridSize = gridSize
+    }
+}
---
+++ GO/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  GO
+//
+//  Created by Eddie Huang on 7/29/18.
+//  Copyright © 2018 Eddie Huang. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
